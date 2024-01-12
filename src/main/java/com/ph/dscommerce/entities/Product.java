bdd9707c3702f5c442d9d235d6802f235df73834
Pedro Henrique <pedro2266809ph@gmail.com>
2024-01-12 18:23:08 -0300

    feat: columnDefininiton para a descrçião

--- src/main/java/com/ph/dscommerce/entities/Product.java
+++ src/main/java/com/ph/dscommerce/entities/Product.java
@@ -13,6 +13,7 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Column(columnDefinition = "TEXT")
     private String description;
     private Double price;
     private String imgUrl;

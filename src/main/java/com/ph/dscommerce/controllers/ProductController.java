c32846f721d298659e2f82dee173dcfc04863d20
Pedro Henrique <pedro2266809ph@gmail.com>
2024-01-15 20:21:33 -0300

    feat> costumizando respostas com ResponseEntity

--- src/main/java/com/ph/dscommerce/controllers/ProductController.java
+++ src/main/java/com/ph/dscommerce/controllers/ProductController.java
@@ -1,13 +1,17 @@
 package com.ph.dscommerce.controllers;
 
 import com.ph.dscommerce.dto.ProductDTO;
+import com.ph.dscommerce.entities.Product;
 import com.ph.dscommerce.services.ProductService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -18,16 +22,24 @@ public class ProductController {
     private ProductService service;
 
     @GetMapping(value = "/{id}")
-    public ProductDTO findById(@PathVariable Long id){
-        return service.findById(id);
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id){
+        ProductDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
     }
     @GetMapping
-    public Page<ProductDTO> findAll(Pageable pageable) {
-        return service.findAll(pageable);
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
+        Page<ProductDTO> dto = service.findAll(pageable);
+        return ResponseEntity.ok(dto);
     }
 
     @PostMapping
-    public ProductDTO insert(@RequestBody ProductDTO productDTO) {
-        return service.insert(productDTO);
+    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO productDTO) {
+        productDTO = service.insert(productDTO);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(productDTO.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(productDTO);
     }
 }

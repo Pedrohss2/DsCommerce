0c255706848ef1f03ad0f830f4475cf13f9abf03
Pedro Henrique <pedro2266809ph@gmail.com>
2024-03-23 15:39:59 -0300

    feat: placed constructor the OrderStatus

--- src/main/java/com/ph/dscommerce/domain/entity/enums/OrderStatus.java
+++ src/main/java/com/ph/dscommerce/domain/entity/enums/OrderStatus.java
@@ -1,10 +1,17 @@
 package com.ph.dscommerce.domain.entity.enums;
 
 public enum OrderStatus {
-    WAITING_PAYMENT,
-    PAID,
-    SHIPPED,
-    DELIVERED,
-    CANCELED
+    WAITING_PAYMENT("WAITING_PAYMENT"),
+    PAID("PAID"),
+    SHIPPED("SHIPPED"),
+    DELIVERED("DELIVERED"),
+    CANCELED("CANCELED");
+
+    private String status;
+
+    OrderStatus(String status) {
+
+        this.status = status;
+    }
 
 }
